6e50dc0a257d4e91a0e6cdb6ac4e563702b66d92
Rainien Kurt Servanez <rainienkurt125@gmail.com>
2024-11-08 17:01:46 +0800

    Commit

--- JavaServanez/src/JavaBasicFundamentals/LoopExercise.java
+++ JavaServanez/src/JavaBasicFundamentals/LoopExercise.java
@@ -9,10 +9,74 @@ import java.util.Scanner;
 
 public class LoopExercise {
 	public static void main(String[] args) {
-		Scanner scan = new Scanner (System.in);
-	
-	
-		System.out.println("error");
-		
+		Scanner scan = new Scanner(System.in);
+
+		// 1 and 100 (even number) increasing order
+
+//
+//		for (int i = 1; i <= 100; i++) {
+//			if (i % 2 == 0)
+//				System.out.print(i + " ");
+//		}
+
+		// 1 and 100 (even number) decreasing order
+
+//		for (int j = 100; j >= 1; j--) {
+//			if (j % 2 != 0)
+//				System.out.print(j + " ");
+//		}
+
+		// Multiplication Table
+
+//		for (int i = 1; i <= 10; i++) {
+//			for (int j = 1; j <= 10; j++)
+//				System.out.print((i * j) + " ");
+//
+//			System.out.println();
+//		}
+
+		// It will stop if the sum of the user input exceeds 100.
+
+//		int sum = 0;
+//		
+//		while(true) {
+//			System.out.print("Number: ");
+//			sum += scan.nextInt();
+//			
+//			if (sum < 100) continue;
+//			
+//			break;
+//		}
+//		System.out.println("Done: " + sum );
+
+		// Maximum and Minimum
+
+//		System.out.print("Enter number: ");
+//		int num = scan.nextInt();
+//		int min = num;
+//		int max = num;
+//
+//		if (num >= 0) {
+//			while (true) {
+//
+//				System.out.print("Enter number: ");
+//				num = scan.nextInt();
+//
+//				if (num < 0)
+//					break;
+//
+//				if (num > max)
+//					max = num;
+//
+//				if (num < min)
+//					min = num;
+//
+//			}
+//
+//			System.out.println("Min: " + min + " Max: " + max);
+//
+//		} else
+//			System.out.println("Invalid input");
+
 	}
 }
